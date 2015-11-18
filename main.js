a683d70fba724b7a0b3fdb70a4f2c0266875fd2d
Josh <jalexy12@gmail.com>
2015-11-18 09:32:12 -0500

    Practice ajax with ironhack characters API

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).on('ready', function(){
+	$.ajax({
+		url: "http://ironhack-characters.herokuapp.com/characters",
+		success: addCharactersToHtml,
+		error: function(){
+			console.log("The request didn't work");
+		}
+	});
+
+	$('.js-new-character').on('submit', function(event){
+		event.preventDefault();
+		var newCharacter = {
+			name: $('.js-name').val(),
+			occupation: $('.js-occupation').val(),
+			weapon: $('.js-weapon').val()
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "http://ironhack-characters.herokuapp.com/characters",
+			data: newCharacter, 
+			success: function(){
+				addSingleCharacterToHtml(newCharacter)
+			},
+			error: function(){
+				console.log("It failed");
+			}
+		})
+
+		$('.js-name').val("")
+		$('.js-occupation').val("")
+		$('.js-weapon').val("")
+	});
+
+	function addSingleCharacterToHtml(character){
+		var info = "<li data-id= " + character.id + ">" + character.name 
+				   + "<br>" + "Debt: " + character.weapon
+				   + "<br>" + character.occupation + "</li>"
+		$('.js-characters-list').append(info);
+	}
+
+	function addCharactersToHtml(characters){
+		// characters.each do | character |
+		// 		puts character
+		// end
+
+		characters.forEach(function(character){
+			console.log(character);
+			addSingleCharacterToHtml(character)
+
+		});
+	}
+});
